b6e92da634cd1e222cc88c8cfc9361dccb91de7f
PhyTakun <dushyantsinghgamer@gmail.com>
2021-07-24 20:16:23 +0530

    React Card

---
+++ Day 16/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./Components/Card";
+import './index.css';
+function App(){
+    return(
+        <div className='App'>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+        
+    )
+}
+
+export default App;
---
+++ Day 16/src/Components/Card.jsx
@@ -0,0 +1,12 @@
+import "./card.css";
+
+const Card = () => {
+    return(
+        <div className='card'>
+            <img src="https://tsh.io/wp-content/uploads/2019/12/react-meme1_.png" alt="" />
+            <h3> A meme :)</h3>
+        </div>
+    )
+}
+
+export default Card;
